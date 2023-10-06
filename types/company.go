89798b31a59d3a491a2392da0054bb053171313d
Andres Court <andres@andrescourt.com>
2023-10-06 06:25:39 -0500

    Add company struct type

--- types/company.go
+++ types/company.go
@@ -1,5 +1,7 @@
 package types
 
+import "github.com/google/uuid"
+
 type CreateCompany struct {
 	ID           string `json:"id"`
 	Name         string `json:"name"`
@@ -7,4 +9,12 @@ type CreateCompany struct {
 	UserEmail    string `json:"user_email"`
 	UserPassword string `json:"user_password"`
 	UserName     string `json:"user_name"`
+}
+
+type Company struct {
+	ID        uuid.UUID `json:"id"`
+	RUC       string    `json:"ruc"`
+	Name      string    `json:"name"`
+	Employees uint8     `json:"employees"`
+	IsActive  bool      `json:"is_active"`
 }
