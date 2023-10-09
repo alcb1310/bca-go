2d5140c82cfb802a16e36b383bc65f57f228e5ab
Andres Court <andres@andrescourt.com>
2023-10-08 20:06:15 -0500

    Created a middleware to run common tasks before each route

---
+++ routes/middleware.go
@@ -0,0 +1,114 @@
+package routes
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/alcb1310/bca-go-w-test/utils"
+	"github.com/google/uuid"
+)
+
+func (s *Router) jsonResponse(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (s *ProtectedRouter) authVerify(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bearerToken := r.Header.Get("Authorization")
+		if bearerToken == "" {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": "Missing autherization token",
+			})
+			return
+		}
+
+		token := strings.Split(bearerToken, " ")
+		if len(token) != 2 {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": "Invalid autherization token",
+			})
+			return
+		}
+
+		secretKey := os.Getenv("SECRET")
+		maker, err := utils.NewJWTMaker(secretKey)
+		if err != nil {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": "Unable to authenticate",
+			})
+			return
+		}
+
+		tokenData, err := maker.VerifyToken(token[1])
+		if err != nil {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": "Unable to authenticate",
+			})
+			return
+		}
+
+		marshalStr, _ := json.Marshal(tokenData)
+		ctx := r.Context()
+		ctx = context.WithValue(r.Context(), "token", marshalStr)
+		r = r.Clone(ctx)
+
+		var t string
+		sql := "SELECT token FROM logged_in_user WHERE user_id = $1"
+		if err := s.db.QueryRow(sql, tokenData.ID).Scan(&t); err != nil {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": "Unable to authenticate",
+			})
+			return
+		}
+
+		if !bytes.Equal([]byte(token[1]), []byte(t)) {
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": "Unable to authenticate",
+			})
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+type contextPayload struct {
+	Id         uuid.UUID `json:"id"`
+	Email      string    `json:"email"`
+	CompanyId  uuid.UUID `json:"company_id"`
+	Role       string    `json:"role"`
+	IsLoggedIn bool      `json:"is_logged_in"`
+	IssuedAt   time.Time `json:"issued_at"`
+	ExpiredAt  time.Time `json:"expired_at"`
+}
+
+func getMyPaload(r *http.Request) (contextPayload, error) {
+	ctx := r.Context()
+	val := ctx.Value("token")
+
+	x, ok := val.([]byte)
+	if !ok {
+		return contextPayload{}, errors.New("Unable to load context")
+	}
+	var p contextPayload
+	err := json.Unmarshal(x, &p)
+	if err != nil {
+		return contextPayload{}, errors.New("Unable to parse context")
+	}
+	return p, nil
+}
