f0ef57bbda953ac4e14cdbcc454b85c6c343d0dd
Andres Court <andres@andrescourt.com>
2023-11-03 09:20:23 -0500

    Display the create project form

--- routes/project.go
+++ routes/project.go
@@ -22,14 +22,41 @@ func (s *settingsRouter) projectsRoutes() {
 	}
 
 	p.HandleFunc("/", p.handleProjects)
+	p.HandleFunc("/crear", p.handleCreateProject)
+}
 
+func (p *proyectRouter) handleCreateProject(w http.ResponseWriter, r *http.Request) {
+	ctxPayload, _ := getMyPaload(r)
+	retData := utils.InitializeMap()
+	retData["UserName"] = ctxPayload.Name
+	retData["Title"] = "BCA - Transacciones"
+
+	switch r.Method {
+	case http.MethodGet:
+		file := append(utils.RequiredFiles, utils.TEMPLATE_DIR+"bca/settings/projects/create-project.html")
+		tmpl, err := template.ParseFiles(file...)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusTeapot)
+			return
+		}
+
+		retData["Projects"], err = p.db.GetAllProjects(ctxPayload.CompanyId)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.ExecuteTemplate(w, "base", retData)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
 
 func (p *proyectRouter) handleProjects(w http.ResponseWriter, r *http.Request) {
 	ctxPayload, _ := getMyPaload(r)
+	retData := utils.InitializeMap()
 	retData["UserName"] = ctxPayload.Name
 	retData["Title"] = "BCA - Transacciones"
-	retData["Links"] = *utils.Links
 
 	switch r.Method {
 	case http.MethodGet:
